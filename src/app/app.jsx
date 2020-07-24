f1121101cadf7c3eaec290fb502b56bc35e97081
Adriano Lemos <adriano.lemos.dev@gmail.com>
2020-07-24 18:03:42 -0300

    Initial template project

---
+++ src/app/app.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.tst.jus.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Conheça o TST
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
