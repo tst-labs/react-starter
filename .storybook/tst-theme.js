80892c25eb8a4082cf592534de7a66e7edb9f799
Adriano Lemos <adriano.lemos.dev@gmail.com>
2020-07-27 17:45:39 -0300

    feature: adds storybook, material-ui, and roboto typefont support

---
+++ .storybook/tst-theme.js
@@ -0,0 +1,13 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  // Typography
+  fontBase: 'Roboto, sans-serif',
+  fontCode: 'monospace',
+
+  brandTitle: 'TST React Starter Storybook',
+  brandUrl: 'https://www.tst.jus.br',
+  brandImage: '/android-chrome-192x192.png',
+});
