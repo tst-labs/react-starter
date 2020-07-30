16d1ef64aa3781da5344d870607d2524b096e7fa
Adriano Lemos <adriano.lemos.dev@gmail.com>
2020-07-29 22:07:57 -0300

    refactor: storybook responsive addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
+import { addParameters } from '@storybook/client-api';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 addDecorator(withA11y);
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
