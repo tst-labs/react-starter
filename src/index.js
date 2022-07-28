4903d0119345404d9dcfb2d9b63566ef35f521ff
Adriano Lemos <adriano.lemos@tst.jus.br>
2022-07-28 15:58:46 -0300

    feat: Atualiza template CRA - (1)
    react, react-dom, react-scripts e adiciona web-vitals

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 
+import reportWebVitals from './utils/reportWebVitals';
 import App from './app/app';
 import * as sw from './utils/pwa/service-worker';
 import appTheme from './utils/ui/app-theme';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={appTheme}>
       <CssBaseline />
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 sw.unregister();
+
+// Se você deseja começar a medir a performance de seu app, passe uma função
+// para registrar os resultados (por example: reportWebVitals(console.log))
+// ou envie-os para um endpoint de dados analíticos (como o Google Analytics)
+// Saiba mais em: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
