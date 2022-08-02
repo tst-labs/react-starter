6bc4ec876a49d9d36b9a0627d6033f9defa93cc5
Adriano Lemos <adriano.lemos@tst.jus.br>
2022-08-02 18:47:31 -0300

    feat: Atualiza o Storybook da versão 5.x para a 6.5
    Ajusta uma das histórias e atualiza um pouco o README.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-storysource',
-    '@storybook/addon-knobs',
-    '@storybook/addon-a11y',
-    '@storybook/addon-viewport/register',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-      },
-    },
-  ],
+  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-storysource', '@storybook/addon-knobs', '@storybook/addon-a11y', '@storybook/addon-viewport/register', {
+    name: '@storybook/addon-docs',
+    options: {
+      configureJSX: true
+    }
+  }],
+  core: {
+    builder: 'webpack5'
+  }
 };
